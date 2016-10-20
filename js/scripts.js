8bbf9493889cd84d65fafed987a0fcd75dcd574e
Brian Pritt and Joshua Rinard <joshua@>
2016-10-20 14:59:38 -0700

    Business Logic, HTML, CSS

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+
+// Business
+var triangleTest = function(s1, s2, s3) {
+  $("#equi, #isos, #scal, #nota").hide();
+
+  if (s1 >= 0 && s2>= 0 && s3 >= 0){
+    if (s1 === s2 && s2 === s3) {
+    return("#equi");
+  } else if (((s1 === s2 && s2 != s3) && (s1&&s2&&s3)> 0)
+          || ((s1 != s2 && s2 === s3 ) &&(s1&&s2&&s3)> 0)
+          ||((s1 === s3 && s2 != s3) && (s1&&s2&&s3)> 0)){
+      return("#isos");
+    } else if ((s1 != s2 && s2 != s3)&&(s1&&s2&&s3) != 0){
+      return("#scal");alert();
+    } else {
+      return("#nota");
+    }
+  } else {
+  alert("Please enter 3 values!");
+}
+};
+
+//Front end
+
+$(document).ready(function(){
+    $("#main form").submit(function(event){
+      var side1Input = parseInt($("#side1").val());
+      var side2Input = parseInt($("#side2").val());
+      var side3Input = parseInt($("#side3").val());
+
+      var triangleType = triangleTest(side1Input, side2Input, side3Input);
+
+      $(triangleType).show();
+
+    event.preventDefault();
+  });
+});
+//((s1 + s2 <= s3)||(s3 + s2 <= s1)||(s3 + s1 <= s2))
