94a7f510cb87e1849e6f67f3259f029826eb6ce9
Brian Pritt and Joshua Rinard <joshua@>
2016-10-20 15:19:48 -0700

    Removes jQuery from business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 
 // Business
 var triangleTest = function(s1, s2, s3) {
-  $("#equi, #isos, #scal, #nota").hide();
+
 
   if (s1 >= 0 && s2>= 0 && s3 >= 0){
     if (s1 === s2 && s2 === s3) {
-    return("#equi");
+    return("#equi");// $("#equi").show();
   } else if (((s1 === s2 && s2 != s3) && (s1&&s2&&s3)> 0)
           || ((s1 != s2 && s2 === s3 ) &&(s1&&s2&&s3)> 0)
           ||((s1 === s3 && s2 != s3) && (s1&&s2&&s3)> 0)){
       return("#isos");
     } else if ((s1 != s2 && s2 != s3)&&(s1&&s2&&s3) != 0){
-      return("#scal");alert();
+      return("#scal");
     } else {
       return("#nota");
     }
@@ -21,7 +21,6 @@ var triangleTest = function(s1, s2, s3) {
 };
 
 //Front end
-
 $(document).ready(function(){
     $("#main form").submit(function(event){
       var side1Input = parseInt($("#side1").val());
@@ -30,9 +29,9 @@ $(document).ready(function(){
 
       var triangleType = triangleTest(side1Input, side2Input, side3Input);
 
+      $("#equi, #isos, #scal, #nota").hide();
       $(triangleType).show();
 
     event.preventDefault();
   });
 });
-//((s1 + s2 <= s3)||(s3 + s2 <= s1)||(s3 + s1 <= s2))
